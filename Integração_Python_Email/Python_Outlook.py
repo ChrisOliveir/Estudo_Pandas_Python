7d16da26e411ff8e945521497c68a153bedb663c
christiane oliveira <christianeolive@hotmail.com>
2022-09-28 21:25:02 -0300

    integração email

---
+++ Integração_Python_Email/Python_Outlook.py
@@ -0,0 +1,15 @@
+﻿import win32com.client as win32
+
+outlook = win32.Dispatch('outlool.application') #instancia do outlook
+
+mail = outlook.CreateItem(0)
+mail.To = 'christianeolive@hotmail.com'
+mail.Subject = "teste"
+mail.Body = 'Texto do E-mail'
+
+#anexos (pode colocar quantos quiser):
+attachment = r'C:\Users\Chris\OneDrive\Área de Trabalho\PandasSql\Financeiro.xlsx'
+mail.Attachments.Add(attachment)
+
+mail.Send()
+print("ok")
