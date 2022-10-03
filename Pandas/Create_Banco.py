3fb6e41e6495b66446e3bcdebb04bcd86b186bf9
christiane oliveira <christianeolive@hotmail.com>
2022-10-02 22:34:14 -0300

    Consultando/Criando no BD

---
+++ Pandas/Create_Banco.py
@@ -0,0 +1,16 @@
+﻿import pandas
+import pyodbc
+dados_conexao = ("Driver={ODBC Driver 17 for SQL Server};Server=DESKTOP-TEO5HBQ\SQLEXPRESS;Database=AdventureWorks;Trusted_connection=yes")
+
+conexao = pyodbc.connect(dados_conexao)
+
+cursor = conexao.cursor()
+
+cursor.execute(""" INSERT INTO HumanResources.Department (Name, GroupName)
+VALUES ('Médico', 'Saúde')
+""")
+cursor.commit()
+print("ok")
+
+cursor.commit()
+cursor.close() # para garntir que finalizou a conexao com o banco para ele nao ficar bloqueado por conta da conexao anterior
