b6ef9a870c9b444f7db74713d2de93289965a420
christiane oliveira <christianeolive@hotmail.com>
2022-10-05 19:35:22 -0300

    Update + Create in sql with Pandas

--- Pandas/Update_banco.py
+++ Pandas/Update_banco.py
@@ -1,16 +1,17 @@
 ﻿import pandas as pd
 import pyodbc
+
 dados_conexao = ("Driver={ODBC Driver 17 for SQL Server};Server=DESKTOP-TEO5HBQ\SQLEXPRESS;Database=AdventureWorks;Trusted_connection=yes")
 
 conexao = pyodbc.connect(dados_conexao)
 
 cursor = conexao.cursor()
 
-cursor.execute("SELECT [Name] FROM [AdventureWorks].[HumanResources].[Department]")
+cursor.execute(''' UPDATE HumanResources.Department SET GroupName='rosa' WHERE DepartmentID=17 ''')
 
-valores = cursor.fetchall()
+cursor.commit()
 
-tabela_valores = pd.DataFrame.from_records(valores)
-print(tabela_valores)
+cursor.close()
+conexao.close()
 
-
+print("ok")
