e7ac071a7f8d799240889f1a586feced44b2f41b
christiane oliveira <christianeolive@hotmail.com>
2022-10-05 19:50:16 -0300

    delete

---
+++ Pandas/Delete_Banco.py
@@ -0,0 +1,19 @@
+﻿import pandas as pd
+import pyodbc
+
+dados_conexao = ("Driver={ODBC Driver 17 for SQL Server};Server=DESKTOP-TEO5HBQ\SQLEXPRESS;Database=AdventureWorks;Trusted_connection=yes")
+
+conexao = pyodbc.connect(dados_conexao)
+
+cursor = conexao.cursor()
+
+cursor.execute(''' DELETE FROM HumanResources.Department WHERE DepartmentID=17 ''')
+
+cursor.commit()
+
+cursor.close()
+conexao.close()
+
+print("ok")
+
+
