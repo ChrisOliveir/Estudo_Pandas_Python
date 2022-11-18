f9905e5baad0f3871da6f3865d3c394c989fa237
christiane oliveira <christianeolive@hotmail.com>
2022-11-18 10:15:37 -0300

    tt

--- Pandas/Read_Banco.py
+++ Pandas/Read_Banco.py
@@ -1,5 +1,6 @@
 ﻿import pandas as pd
 import pyodbc
+from openpyxl import Workbook
 dados_conexao = ("Driver={ODBC Driver 17 for SQL Server};Server=DESKTOP-TEO5HBQ\SQLEXPRESS;Database=AdventureWorks;Trusted_connection=yes")
 
 conexao = pyodbc.connect(dados_conexao)
@@ -11,6 +12,14 @@ cursor.execute("SELECT [Name] FROM [AdventureWorks].[HumanResources].[Department
 valores = cursor.fetchall()
 
 tabela_valores = pd.DataFrame.from_records(valores)
-print(tabela_valores)
 
+
+wb = Workbook()
+ws = wb.active
+ws.cell(1, 1, "CARGO")
+wb.save('C:/Users/Chris/OneDrive/tabela_valores.xlsx')
+
+
+
+#print(tabela_valores)
 
